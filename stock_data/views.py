c4ec30108bc30cb6502597d123a624c5f61da1fc
David Hoffmann <dave@dhoff.net>
2021-02-20 17:27:43 -0600

    updates sto ui

--- stock_data/views.py
+++ stock_data/views.py
@@ -1,5 +1,7 @@
 import requests
 import json 
+import datetime
+from datetime import timedelta, date
 from yahoo_fin import stock_info as si
 
 from django.shortcuts import render
@@ -25,13 +27,40 @@ def index(request):
     twitter_data = si.get_quote_table(twitter, dict_result = True)
     ihub_data = si.get_quote_table(ihub, dict_result = True)
 
+    now = datetime.datetime.now()
+    then = start_date = datetime.datetime.now() - datetime.timedelta(30)
+    month_data = si.get_data(reddit, then, now)
+    open_data = []
+    print(month_data)
+    for data in month_data.open:
+        open_data.append(data)
+
+    print(open_data)
+    print(len(open_data))
+    start_dt = date(2019,1,21)
+    end_dt = date(2019,2,4)
+
+    data_date = []
+    weekdays = [5,6]
+    for dt in daterange(now, then):
+        if dt.weekday() not in weekdays:
+            data_date.append(dt.strftime("%Y-%m-%d"))
+            print(dt.strftime("%Y-%m-%d"))
+    print(data_date)
     context = {
         'reddit': reddit,
         'ihub': ihub,
         'ihub_data': ihub_data,
         'twitter': twitter,
         'twitter_data': twitter_data,
-        'reddit_data': reddit_data
+        'reddit_data': reddit_data,
+        'open_data': open_data,
+        'data_date': data_date,
         
     }
     return render(request, "stock_data/screener.html", context)
+
+
+def daterange(date1, date2):
+    for n in range(int ((date2 - date1).days)+1):
+        yield date1 + timedelta(n)
